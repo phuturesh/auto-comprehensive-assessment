c3ccf2f13a939c82a30edb6149e985f86b0cdad5
cona <945693963@qq.com>
2023-08-25 12:46:12 +0800

    fully automatized

--- auto_fill.py
+++ auto_fill.py
@@ -12,18 +12,22 @@ driver.get("http://172.31.126.2/login?redirect=/user/profile")
 
 username=driver.find_element(by="xpath",value="/html/body/div/div/form/div[1]/div/div/input")
 password = driver.find_element(by="xpath",value="/html/body/div/div/form/div[2]/div/div[1]/input")
+driver.implicitly_wait(2)
+# 下面输入你的学号，后续不需要输验证码
+stu_id = "302xxxxxxx"
 
-# 下面两行输入你的用户名和密码，两者皆输入你的学号，后续不需要输验证码
 username.clear()
 password.clear()
-username.send_keys("")
-password.send_keys("")
-# 你有20秒完成: 点击 ，登录, 点击左侧"班级互评"按钮, 如果不够, 请修改sleep后面的数字(秒)
-print("waiting for logging in")
-time.sleep(20)
-driver.implicitly_wait(2)
+username.send_keys(stu_id)
+password.send_keys(stu_id)
 
-grades = ["25", "45", "10", "10", "10"]
+login_button = driver.find_element(by=By.CLASS_NAME, value="el-button--large")
+login_button.click()
+cross_vali_button = driver.find_elements(by=By.CLASS_NAME, value="submenu-title-noDropdown")[1]
+cross_vali_button.click()
+
+# 打分列表, 依次为德智体美劳
+grades = ["20", "45", "10", "10", "10"]
 # 一轮只能处理一个页面的同学
 def fill_in_one_page(page):
     for i in range(0, 10):
